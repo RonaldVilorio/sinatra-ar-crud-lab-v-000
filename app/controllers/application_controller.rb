f75b8d1b98c27f1081e381ed201c3119abeaa98c
ronald vilorio <soccer23415@gmail.com>
2018-01-24 05:30:08 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,8 +39,7 @@ class ApplicationController < Sinatra::Base
     @post.name = params[:name]
     @post.content = params[:content]
     @post.save
-    id = @post.id.to_s
-    redirect '/posts/id'
+    redirect '/posts/2'
   end
 
 
