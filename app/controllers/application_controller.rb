3840b74b716bcfe775b62232118418344f652f7c
ronald vilorio <soccer23415@gmail.com>
2018-01-24 05:19:07 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
     @post = Post.find(params[:id])
     @post.name = params[:name]
     @post.content = params[:content]
-    @post.update
+    @post.save
     erb :show
   end
 
